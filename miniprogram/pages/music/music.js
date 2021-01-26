0392e8ae92d029b69d113498ce2dd5a0a300c069
dinglihui <156963866@qq.com>
2021-01-26 20:37:45 +0800

    1 26

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -6,7 +6,44 @@ Page({
    */
   data: {
 
+    imgUrls:[{
+      url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+    },
+  
+  ],
+    playlist:[{
+    "id":"1001",
+    "playCount": 1.4641238e+06,
+    "name": "心动演绎 | 国人唱英文 申请潺潺如飞",
+    "picUrl":"https://music.163.com/playlist?id=5397034894"
   },
+     {
+    "id":"1002",
+    "playCount": 1.4641238e+06,
+    "name": "心动演绎 | 国人唱英文 申请潺潺如飞",
+    "picUrl":"https://music.163.com/playlist?id=5397034894"
+     },
+
+     {
+      "id":"1003",
+      "playCount": 1.4641238e+06,
+      "name": "心动演绎 | 国人唱英文 申请潺潺如飞",
+      "picUrl":"https://music.163.com/playlist?id=5397034894"
+       },
+  
+
+  ],
+},
+
 
   /**
    * 生命周期函数--监听页面加载
