6d1e557592eb4cd2e621d6dc28e6227c5b6797fd
dinglihui <156963866@qq.com>
2021-01-27 19:50:36 +0800

    1 27

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,19 +1,13 @@
 //app.js
 App({
-  onLaunch: function (option) {
+  onLaunch: function (options) {
     console.log('onLaunch 执行')
-    console.log(options)
-    
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        //'my-env-id',
-         env:'ding-8g6gxqq5bf48c88c',
+        env: 'lalajie-7geohod12576cf3b',
         traceUser: true,
       })
     }
@@ -21,8 +15,8 @@ App({
     this.globalData = {}
   },
 
-  onShow(options){
+  onShow(options) {
     console.log('onShow 执行')
-    console.log(option)
+    console.log(options)
   }
 })
--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -1,55 +1,72 @@
 // pages/music/music.js
+const MAX_LIMIT = 15
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-    imgUrls:[{
-      url:'http://p1.music.126.net/nVUH7O5ZNMG1OQ1kE-tq9g==/109951165665595417.jpg?imageView&quality=89'
-    },
-    {
+    imgUrls: [{
+      url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
+    },{
       url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
-    },
-    {
+    },{
       url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
-    },
-    {
+    },{
       url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
-    },
-  
-  ],
-    playlist:[{
-    "id":"1001",
-    "playCount": 1.4641238e+06,
-    "name": "心动演绎 | 国人唱英文 申请潺潺如飞",
-    "picUrl":"https://music.163.com/playlist?id=5397034894"
+    },{
+      url:'http://p1.music.126.net/zUv2mRobckK7Tdn2bp9iSA==/109951165664840470.jpg?imageView&quality=89'
+    },{
+      url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+    },{
+      url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+    },{
+      url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+    },{
+      url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+    },{
+      url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+    }],
+     playlist: [
+     {
+      "id": "1001",
+      "playCount":1.461238e+06,
+      "name": "一秒沉醉 • 欧美迷情仙女嗓",
+      "picUrl": "http://p4.music.126.net/Hr8sEJzhAKvKo8x56n-Yhw==/109951165525778915.jpg?param=140y140"
+    },{
+      "id": "1002",
+      "playCount":62282.6,
+      "name": "欧美流行歌曲｜好听到单曲循环",
+      "picUrl": "http://p4.music.126.net/NM-wa_-u-H-fWrwk1CCg9w==/109951165329812130.jpg?param=140y140"
+    },{
+      "id": "1003",
+      "playCount":1.712352e+06,
+      "name": "春昼短 剧本杀歌单",
+      "picUrl": "http://p3.music.126.net/7i8Iz1Faq4fRG8QI7J72ng==/109951165570057766.jpg?param=140y140"
+    },{
+      "id": "1004",
+      "playCount":6.586255e+06,
+      "name": "2020年度最热欧美新歌TOP50",
+      "picUrl": "http://p3.music.126.net/oIJhYBUIOkrDZxferlNHkA==/109951165589620531.jpg?param=140y140"
+    },{
+      "id": "1005",
+      "playCount":1.06578992e+08,
+      "name": "希望开心和好运奔向我 与我撞个满怀",
+      "picUrl": "http://p3.music.126.net/neFYif9QejB_sygh8zInwg==/109951165558451625.jpg?param=140y140"
+    },{
+      "id": "1006",
+      "playCount":1.574256e+6,
+      "name": "在没有相见的日子里，请你万分保重",
+      "picUrl": "http://p4.music.126.net/1u0qwg87O8-IZwO3h42kQg==/109951165409243905.jpg?param=140y140"
+    }]
   },
-     {
-    "id":"1002",
-    "playCount": 1.4641238e+06,
-    "name": "心动演绎 | 国人唱英文 申请潺潺如飞",
-    "picUrl":"https://music.163.com/playlist?id=5397034894"
-     },
-
-     {
-      "id":"1003",
-      "playCount": 1.4641238e+06,
-      "name": "心动演绎 | 国人唱英文 申请潺潺如飞",
-      "picUrl":"https://music.163.com/playlist?id=5397034894"
-       },
-  
-
-  ],
-},
-
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this._getPlaylist()
   },
 
   /**
@@ -97,7 +114,20 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function () {},
 
-  }
+  _getPlaylist() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.cloud.callFunction({
+      name: 'playlist'
+    }).then((res) => {
+      console.log(res)
+      this.setData({
+        playlist: res.result
+      })
+      wx.hideLoading()
+    })
+  },
 })
